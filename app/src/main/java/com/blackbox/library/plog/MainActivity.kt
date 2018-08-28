718e3c5ae43dc5c47b863ad3fd0460a74bcb983d
Umair Adil <mua@embrace-it.com>
2018-08-28 16:00:55 +0500

    Added encryption support.

--- app/src/main/java/com/blackbox/library/plog/MainActivity.kt
+++ app/src/main/java/com/blackbox/library/plog/MainActivity.kt
@@ -7,12 +7,14 @@ import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
-import com.blackbox.plog.dataLogs.DataLogBuilder
 import com.blackbox.plog.dataLogs.DataLogger
-import com.blackbox.plog.pLogs.LogFormatter
+import com.blackbox.plog.dataLogs.models.DataLogBuilder
 import com.blackbox.plog.pLogs.PLog
-import com.blackbox.plog.pLogs.PLogBuilder
+import com.blackbox.plog.pLogs.formatter.LogFormatter
+import com.blackbox.plog.pLogs.formatter.TimeStampFormat
+import com.blackbox.plog.pLogs.models.PLogBuilder
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
@@ -23,31 +25,40 @@ class MainActivity : AppCompatActivity() {
 
     val TAG: String = MainActivity::class.java.simpleName
     var PERMISSION_CODE = 9234;
+    var ENCRYPTION_KEY = "23233526436245232364264262343243"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val logsPath = Environment.getExternalStorageDirectory().absolutePath + File.separator + "PLogs"
+
         //This must be initialized before calling PLog
         PLogBuilder()
-                .setLogsSavePath(Environment.getExternalStorageDirectory().absolutePath + File.separator + "PLogTest")
-                .setLogsExportPath(Environment.getExternalStorageDirectory().absolutePath + File.separator + "PLogTest" + File.separator + "ZippedLogs")
+                .setLogsSavePath(logsPath)
+                .setLogsExportPath(logsPath + File.separator + "ZippedLogs")
                 .debuggable(true)
                 .setLogFileExtension(".txt")
                 .setLogFormatType(LogFormatter.FORMAT_CURLY)
                 .attachTimeStampToFiles(true)
-                .setTimeStampFormat("dd MMMM yyyy kk:mm:ss")
+                .setTimeStampFormat(TimeStampFormat.DATE_FORMAT_1)
+                .enableEncryption(true) //Enable Encryption
+                .setEncryptionKey(ENCRYPTION_KEY) //Set Encryption Key
+                .enabled(true)
                 .build()
 
         //This must be initialized before calling DataLogger
         //Each DataLogger builder can be used to log different data files
         val myLogs: DataLogger = DataLogBuilder()
-                .setLogsSavePath(Environment.getExternalStorageDirectory().absolutePath + File.separator + "DataLogTest")
-                .setLogsExportPath(Environment.getExternalStorageDirectory().absolutePath + File.separator + "DataLogTest" + File.separator + "ZippedLogs")
+                .setLogsSavePath(logsPath)
+                .setLogsExportPath(logsPath + File.separator + "ZippedLogs")
                 .setLogFileName("myLogs.txt")
                 .setExportFileName("myLogsExported")
                 .attachTimeStampToFiles(false)
                 .debuggable(true)
+                .enableEncryption(true) //Enable Encryption
+                .setEncryptionKey(ENCRYPTION_KEY) //Set Encryption Key
+                .enabled(true)
                 .build()
 
 
@@ -84,33 +95,64 @@ class MainActivity : AppCompatActivity() {
         export.setOnClickListener {
 
             //Will export PLogs
-            PLog.getLogs(PLog.LOG_TODAY)
+            PLog.getZippedLog(PLog.LOG_TODAY)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeBy(  // named arguments for lambda Subscribers
+                    .subscribeBy(
                             onNext = {
                                 PLog.logThis(TAG, "exportPLogs", "PLogs Path: $it", PLog.TYPE_INFO)
                                 Toast.makeText(this@MainActivity, "Exported to: $it", Toast.LENGTH_SHORT).show()
                             },
                             onError = {
                                 it.printStackTrace()
-                                PLog.logThis(TAG, "exportPLogs", "Error: " + it.message, PLog.TYPE_ERROR)
+                                PLog.logThis(TAG, "exportPLogs", "PLog Error: " + it.message, PLog.TYPE_ERROR)
                             },
                             onComplete = { }
                     )
 
             //Will Export custom data log
-            myLogs.logs
+            myLogs.getZippedLogs()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeBy(  // named arguments for lambda Subscribers
+                    .subscribeBy(
                             onNext = {
                                 PLog.logThis(TAG, "exportDataLogs", "DataLog Path: $it", PLog.TYPE_INFO)
                                 Toast.makeText(this@MainActivity, "Exported to: $it", Toast.LENGTH_SHORT).show()
                             },
                             onError = {
                                 it.printStackTrace()
-                                PLog.logThis(TAG, "exportDataLogs", "Error: " + it.message, PLog.TYPE_ERROR)
+                                PLog.logThis(TAG, "exportDataLogs", "DataLogger Error: " + it.message, PLog.TYPE_ERROR)
+                            },
+                            onComplete = { }
+                    )
+        }
+
+        printLogs.setOnClickListener {
+
+            PLog.getLoggedData(PLog.LOG_TODAY)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy(
+                            onNext = {
+                                Log.i("PLog", "$it")
+                            },
+                            onError = {
+                                it.printStackTrace()
+                                PLog.logThis(TAG, "printLogs", "PLog Error: " + it.message, PLog.TYPE_ERROR)
+                            },
+                            onComplete = { }
+                    )
+
+            myLogs.getLoggedData()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy(
+                            onNext = {
+                                Log.i("DataLog", "$it")
+                            },
+                            onError = {
+                                it.printStackTrace()
+                                PLog.logThis(TAG, "printLogs", "DataLogger Error: " + it.message, PLog.TYPE_ERROR)
                             },
                             onComplete = { }
                     )
